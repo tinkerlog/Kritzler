16ce0a335b0cda9efb4e5e5cdd3b327f074a06ff
alex <alex@tinkerlog.com>
2017-07-11 16:31:41 +0200

    add

--- plotter.sh
+++ plotter.sh
@@ -1,4 +1,5 @@
 
 export CP=lib/RXTXcomm.jar:lib/core.jar:lib/serial.jar:bin:lib/geomerative.jar:lib/controlP5.jar
 
+# java -d32 -Djava.library.path=lib -cp $CP com.tinkerlog.kritzler.Plotter
 java -d32 -Djava.library.path=lib -cp $CP com.tinkerlog.kritzler.Plotter
